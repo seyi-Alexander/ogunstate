fb8686640029b400bdaa4f53c6e215f27dd24926
seyi-Alexander <seyistry@gmail.com>
2018-01-23 22:13:11 +0100

    Update index.php

--- index.php
+++ index.php
@@ -16,7 +16,7 @@
         <img class="header__logo" src="images/city.png" alt="iconic view of a cityscape">
         <h1 class="header__title">
           Association of Nigeria Nurse Educator
-        </h1><h3 class="header__title"> Ogun State Chapter</h3>
+        </h1><h3 > Ogun State Chapter</h3>
         <a id="menu" class="header__menu">
           <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
             <path d="M2 6h20v3H2zm0 5h20v3H2zm0 5h20v3H2z"/>
